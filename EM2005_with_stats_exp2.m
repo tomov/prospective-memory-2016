2134c55f7cdafdde53a64109c3759515d9e7a3be
Momchil Tomov <momchil@memsql.com>
2016-07-28 17:55:05 -0700

    wtf is going on here...

--- EM2005_with_stats_exp2.m
+++ EM2005_with_stats_exp2.m
@@ -115,9 +115,8 @@ empirical_RTs = empirical_stats(:, 4);
 % Focal, PM and the Focal, No-PM condition same as the Nonfocal, No-PM
 % condition
 % a little fraud b/c the Focal, PM is slightly above the No-PM conditions generally 
-RTs_to_ignore_when_fitting = ...
-    (empirical_stats(:, 1) == 1 & empirical_stats(:, 2) == 1); %...
-   % | (empirical_stats(:, 1) == 0 & empirical_stats(:, 2) == 1);
+RTs_to_ignore_when_fitting = empirical_stats(:, 1) == 1 & empirical_stats(:, 2) == 1;
+%RTs_to_ignore_when_fitting = RTs_to_ignore_when_fitting | (empirical_stats(:, 1) == 0 & empirical_stats(:, 2) == 1);
 empirical_RTs = empirical_RTs(~RTs_to_ignore_when_fitting);
 simulation_cycles = simulation_cycles(~RTs_to_ignore_when_fitting);
 

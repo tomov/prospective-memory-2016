c07f5d754432961896870a6175e61cfc7bdeaf9e
Momchil Tomov <momchil@memsql.com>
2016-08-04 17:08:48 -0700

    SEM's for FIGURE_11

--- FIGURE_11.m
+++ FIGURE_11.m
@@ -3,11 +3,6 @@
 EM2005_with_stats_exp1
 
 
-
-
-
-
-
 Ms = zeros(1, 2);
 SEMs = zeros(1, 2);
 for FOCAL = 1:-1:0
@@ -24,8 +19,18 @@ end
 
 figure;
 
+
+
 subplot(3, 2, 1);
-barweb([1145.63 1335.73], [0 0]/sqrt(subjects_per_condition), 1, {}, ...
+focal_ms = empirical_stats(empirical_stats(:, 2) == 1, 4);
+focal_sds = empirical_stats(empirical_stats(:, 2) == 1, 5);
+assert(abs(mean(focal_ms) - 1145.63) < 0.1); % sanity check b/c we compute the SD's like this
+nonfocal_ms = empirical_stats(empirical_stats(:, 2) == 0, 4);
+nonfocal_sds = empirical_stats(empirical_stats(:, 2) == 0, 5);
+assert(abs(mean(nonfocal_ms) - 1335.73) < 0.1); % sanity check b/c we compute the SD's like this
+focal_sd = sqrt(sum(focal_sds .^ 2));
+nonfocal_sd = sqrt(sum(nonfocal_sds .^ 2));
+barweb([1145.63 1335.73], [focal_sd nonfocal_sd]/sqrt(subjects_per_condition), 1, {}, ...
     'Empirical Data', 'PM Condition', 'Ongoing RT (msec)');
 h = legend({'Focal', 'Nonfoc'});
 set(h, 'FontSize', 10);
@@ -58,7 +63,15 @@ for EMPHASIS = 0:1
 end
 
 subplot(3, 2, 3);
-barweb([1190.11 1291.26], [0 0]/sqrt(subjects_per_condition), 1, {}, ...
+low_ms = empirical_stats(empirical_stats(:, 3) == 0, 4);
+low_sds = empirical_stats(empirical_stats(:, 3) == 0, 5);
+assert(abs(mean(low_ms) - 1190.11) < 0.1); % sanity check b/c we compute the SD's like this
+high_ms = empirical_stats(empirical_stats(:, 3) == 1, 4);
+high_sds = empirical_stats(empirical_stats(:, 3) == 1, 5);
+assert(abs(mean(high_ms) - 1291.26) < 0.1); % sanity check b/c we compute the SD's like this
+low_sd = sqrt(sum(low_sds .^ 2));
+high_sd = sqrt(sum(high_sds .^ 2));
+barweb([1190.11 1291.26], [low_sd high_sd]/sqrt(subjects_per_condition), 1, {}, ...
     '', 'PM Condition', 'Ongoing RT (msec)');
 h = legend({'Low', 'High'});
 set(h, 'FontSize', 10);
